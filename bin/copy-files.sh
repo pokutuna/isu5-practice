affc3975204ced4106be3be50a50af1994bb57f1
pokutuna <popopopopokutuna@gmail.com>
2016-09-10 18:14:24 +0900

    make symlinks in copy-files

--- bin/copy-files.sh
+++ bin/copy-files.sh
@@ -4,6 +4,10 @@ cd $(dirname $0)
 cd .. # /home/isucon/webapp/perl
 # root
 
+ln -sf /home/isucon/webapp/perl/config/nginx.conf /etc/nginx/nginx.conf
+ln -sf /home/isucon/webapp/perl/config/my.cnf /etc/my.cnf
+ln -sf /home/isucon/webapp/perl/config/sysctl.conf /etc/sysctl.conf
+
 cp -r script /home/isucon/webapp/
 cp -r static /home/isucon/webapp/
 cp -r sql /home/isucon/webapp/
