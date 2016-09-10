b933837c0836867e0ad617336c04995345ec389b
pokutuna <popopopopokutuna@gmail.com>
2016-09-10 16:56:40 +0900

    footprints 引くクエリ改善

--- lib/Isucon5/Web.pm
+++ lib/Isucon5/Web.pm
@@ -435,11 +435,10 @@ post '/diary/comment/:entry_id' => [qw(set_global authenticated)] => sub {
 get '/footprints' => [qw(set_global authenticated)] => sub {
     my ($self, $c) = @_;
     my $query = <<SQL;
-SELECT user_id, owner_id, DATE(created_at) AS date, MAX(created_at) as updated
+SELECT user_id, owner_id, created_at as updated
 FROM footprints
 WHERE user_id = ?
-GROUP BY user_id, owner_id, DATE(created_at)
-ORDER BY updated DESC
+ORDER BY created_at DESC
 LIMIT 50
 SQL
     my $footprints = [];
