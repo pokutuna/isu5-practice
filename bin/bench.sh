6d3d6988f92f3f561d634b29ad56fd75f0ac86fc
pokutuna <popopopopokutuna@gmail.com>
2016-09-15 23:12:01 +0900

    iroiro

--- bin/bench.sh
+++ bin/bench.sh
@@ -3,6 +3,7 @@
 BENCH_HOST=${BENCH_HOST:-"13.78.88.53"}
 IMAGE_HOST=${IMAGE_HOST:-"13.78.88.61"}
 
+ssh -t isucon@$IMAGE_HOST "cd /home/isucon/webapp/perl && ./bin/init.sh"
 ssh -t isucon@$BENCH_HOST "cd /home/isucon && ./bench.sh ${IMAGE_HOST}"
 ssh -t isucon@$IMAGE_HOST "cd /home/isucon/webapp/perl && ./bin/alp.sh"
 
