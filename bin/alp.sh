c5e9e4157cb4299628e613bd1ded7e9070146b3f
pokutuna <popopopopokutuna@gmail.com>
2016-09-15 23:02:55 +0900

    iroiro

--- bin/alp.sh
+++ bin/alp.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-/usr/local/bin/alp -f /var/log/nginx/access.log
 
+# /usr/local/bin/alp -f /var/log/nginx/access.log
+/usr/local/bin/alp -f ~/var/log/nginx/access.log \
+  --aggregates "/diary/entries/.*,/profile/.*,/friends/.*,/diary/entry/\d+,/diary/comment/\d+"
--- bin/bench.sh
+++ bin/bench.sh
@@ -4,5 +4,6 @@ BENCH_HOST=${BENCH_HOST:-"13.78.88.53"}
 IMAGE_HOST=${IMAGE_HOST:-"13.78.88.61"}
 
 ssh -t isucon@$BENCH_HOST "cd /home/isucon && ./bench.sh ${IMAGE_HOST}"
+ssh -t isucon@$IMAGE_HOST "cd /home/isucon/webapp/perl && ./bin/alp.sh"
 
 # TODO notify to slack
